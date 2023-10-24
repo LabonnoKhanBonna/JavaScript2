1bb8f89518104d5eca570001b3e41f18dbb60d00
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-10-24 11:49:56 +0600

    my second commit

---
+++ let-const.js
@@ -0,0 +1,28 @@
+//var x = "hello"
+
+//x = "world"
+
+//console.log(x) 
+
+//let x = "hello"
+//const y = "world"
+
+//x = "mehedy"
+//x = "Linkon"
+//console.log(x)
+
+//y = "hasan"
+//console.log(y)
+
+const name = "Labonno"
+//const result = name.includes("li");
+const result = name.includes("Lab");
+console.log(result)
+
+const pets = ['cat', 'dog','rat'];
+const results = pets.includes("dog");
+console.log(results)
+
+const animale = ["ant", "elephanat" , "dog", "cat", "duck","camel", "cow" ]
+console.log(animale.slice(2))
+console.log(animale.slice(2,4))
